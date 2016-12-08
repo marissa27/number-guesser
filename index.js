da073fe504a00e14dfaabfb529daf58e45841d58
Marissa Reinke <reinke.marissa@gmail.com>
2016-12-07 23:01:25 -0700

    Setup CSS

--- index.js
+++ index.js
@@ -3,23 +3,83 @@ var userInput = document.getElementById('user-input');
 var messageInput = document.getElementById('guess-number');
 var submitBtn = document.getElementById("submit-btn");
 var clearInput = document.querySelector(".clear-btn");
+var resetBtn = document.querySelector(".reset-btn");
+var userResult = document.querySelector('.user-result');
+var resultText = document.querySelector('.result-text');
+var min = 0;
+var max = 100;
+var minInput = document.querySelector(".min");
+var maxInput = document.querySelector(".max");
 
 // Sets return message for number guess results
 submitBtn.addEventListener('click', function () {
-  var number = parseInt(userInput.value);
-  document.querySelector('.user-result').innerText = number;
-  console.log(userInput.value);
-  if(number > randomNumber && number < 100) {
-    document.querySelector('.result-text').innerText = "That is too high!";
-  } else if(number < randomNumber && number > 0) {
-    document.querySelector('.result-text').innerText = "That is too low!";
-  } else if(number === randomNumber) {
-    document.querySelector('.result-text').innerText = "BOOM!";
-  } else {
-    document.querySelector('.result-text').innerText = "ERROR: Please enter a value from 1 to 100";
-  }
+  checkWithinRange();
 });
 
+function checkWithinRange(){
+  var number = parseInt(userInput.value);
+  userResult.innerText = number;
+  // console.log(userInput.value);
+if (number > randomNumber && number < max) {
+    resultText.innerText = "That is too high!";
+  } else if(number < randomNumber && number > min) {
+    resultText.innerText = "That is too low!";
+  } else if(number === randomNumber) {
+    resultText.innerText = "BOOM!";
+    numberValues();
+  } else {
+    resultText.innerText = "ERROR: Please enter a value from " + min + " to " + max;
+  }
+}
+
+// function getNum(val) {
+//    if (isNaN(val)) {
+//      userResult.innerText = '#';
+//    }
+//    userResult.innerText = val;
+// }
+
+function numberValues() {
+  min = min - 10;
+  max =  max + 10;
+  minInput.value = min;
+  maxInput.value = max;
+  randomNumber = changeRange(min, max);
+}
+
+// Clears User Input
 clearInput.addEventListener('click', function () {
   userInput.value = "";
 })
+
+// Reset Button
+resetBtn.addEventListener('click', function () {
+  userInput.value = "";
+  randomNumber = Math.floor(Math.random() * 100 +1);
+  userResult.innerText = "";
+  resultText.innerText = "";
+  minInput.value = "0";
+  maxInput.value = "100";
+})
+
+//add event listener
+userInput.addEventListener('input', function(event) {
+    resetBtn.disabled = false;
+    clearInput.disabled = false;
+});
+
+//Change Range
+minInput.addEventListener('keyup', function (){
+  min = parseInt(minInput.value);
+  randomNumber = changeRange(min, max);
+})
+
+maxInput.addEventListener('keyup', function (){
+  max = parseInt(maxInput.value);
+  randomNumber = changeRange(min, max);
+})
+
+function changeRange (min, max)  {
+  // console.log(min, max);
+ return Math.floor(Math.random() * (max - min)) + min;
+}
